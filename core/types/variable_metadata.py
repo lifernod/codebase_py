f6b05eb6a5fff1af8e8efef37ba46db0b0f881b7
niikage <lifernod@gmail.com>
2026-05-11 22:00:37 +0700

    remove line_start and line_end from variable

--- core/types/variable_metadata.py
+++ core/types/variable_metadata.py
@@ -4,8 +4,6 @@ from dataclasses import dataclass, field
 @dataclass
 class VariableMetadata:
     name: str
-    line_start: int = field(default=0)
-    line_end: int = field(default=0)
     ty: str | None = field(default=None)
 
     def __str__(self):
